6028fd2ea6dada1b57eccc91f482e7f8d8fb1421
Antonio Nicolás Salmerón Rubio <54323367+S3ll4rd0@users.noreply.github.com>
2022-05-16 21:28:00 +0200

    Añado el código al evento click del botón del Form2 y el texto que tiene que mostrar. Compruebo que funciona y commit.

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -28,46 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.lblForm2Texto = new System.Windows.Forms.Label();
+            this.lblForm2Título = new System.Windows.Forms.Label();
+            this.btnForm2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // lblForm2Texto
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(170, 231);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.lblForm2Texto.AutoSize = true;
+            this.lblForm2Texto.Location = new System.Drawing.Point(27, 196);
+            this.lblForm2Texto.Name = "lblForm2Texto";
+            this.lblForm2Texto.Size = new System.Drawing.Size(0, 16);
+            this.lblForm2Texto.TabIndex = 1;
             // 
-            // label2
+            // lblForm2Título
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(173, 76);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.lblForm2Título.AutoSize = true;
+            this.lblForm2Título.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblForm2Título.Location = new System.Drawing.Point(25, 66);
+            this.lblForm2Título.Name = "lblForm2Título";
+            this.lblForm2Título.Size = new System.Drawing.Size(0, 25);
+            this.lblForm2Título.TabIndex = 2;
             // 
-            // button1
+            // btnForm2
             // 
-            this.button1.Location = new System.Drawing.Point(173, 150);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnForm2.Location = new System.Drawing.Point(69, 145);
+            this.btnForm2.Name = "btnForm2";
+            this.btnForm2.Size = new System.Drawing.Size(300, 25);
+            this.btnForm2.TabIndex = 3;
+            this.btnForm2.Text = "¡Apreta\'m i vorás a La Vila plorar!";
+            this.btnForm2.UseVisualStyleBackColor = true;
+            this.btnForm2.Click += new System.EventHandler(this.btnForm2_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(382, 353);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(432, 353);
+            this.Controls.Add(this.btnForm2);
+            this.Controls.Add(this.lblForm2Título);
+            this.Controls.Add(this.lblForm2Texto);
             this.Name = "Form2";
             this.Text = "Formulario 2 - Tonet S3ll4rd0";
             this.ResumeLayout(false);
@@ -77,8 +77,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblForm2Texto;
+        private System.Windows.Forms.Label lblForm2Título;
+        private System.Windows.Forms.Button btnForm2;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -16,5 +16,13 @@ namespace PruebaEx16
         {
             InitializeComponent();
         }
+
+        private void btnForm2_Click(object sender, EventArgs e)
+        {
+            lblForm2Título.Text = "¿De La Vila i plores, cabra fotuda?";
+            lblForm2Texto.Text = "Interpel·lar algú de la Vila Joiosa amb aquesta pregunta \n" +
+                                 "és una petita ofensa: una ofensa còmplice, venial; \n" +
+                                 "a penes una temptativa d'ofensa, si es vol. Però una ofensa.";
+        }
     }
 }
